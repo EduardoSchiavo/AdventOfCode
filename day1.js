fc919817e1dc70cab642578239e5290463e497b1
Eduardo <eduardoschiavo09@gmail.com>
2022-12-01 16:12:26 +0100

    js refactoring

--- day1.js
+++ day1.js
@@ -1,16 +1,11 @@
 const {readFileSync, promises: fsPromises} = require('fs');
 
-filename="examples/example1.txt"
+fileName="examples/example1.txt"
 
-const contents = readFileSync(filename, 'utf-8').split("\n\n");
-
-let load_per_elf = []
-
-contents.forEach((item) => load_per_elf.push(item.split('\n').map(Number).reduce((a,b)=>a+b)))
+const loadPerElf = readFileSync(fileName, 'utf-8').split("\n\n").map((item) => (item.split('\n').map(Number).reduce((a,b)=>a+b)));
 
 // P1
-console.log("Part one:", Math.max(...load_per_elf))
+console.log("Part one:", Math.max(...loadPerElf));
 
 // P2
-
-console.log(load_per_elf.sort((a,b)=>b-a).slice(0,3).reduce((a,b)=>a+b))
+console.log("Part two:",loadPerElf.sort((a,b)=>b-a).slice(0,3).reduce((a,b)=>a+b));
--- day1.py
+++ day1.py
@@ -1,11 +1,11 @@
 with open('examples/example1.txt') as ifile:
     inp=ifile.read().split('\n\n')
 
-loads_per_elf=[sum([int(e) for e in elf]) for elf in [x.split() for x in inp]]
+load_per_elf=[sum([int(e) for e in elf]) for elf in [x.split() for x in inp]]
 
 # P1
-print("part 1: ", max(loads_per_elf))
+print("part 1: ", max(load_per_elf))
 
-loads_per_elf.sort()
+load_per_elf.sort()
 # P2
-print("part 2: ", sum(loads_per_elf[-3:]))
+print("part 2: ", sum(load_per_elf[-3:]))
