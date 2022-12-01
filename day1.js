7252c8dcf2e197f18c19068c348a4e4ff6602a2d
Eduardo <eduardoschiavo09@gmail.com>
2022-12-01 16:17:12 +0100

    separate read

--- day1.js
+++ day1.js
@@ -2,7 +2,9 @@ const {readFileSync} = require('fs');
 
 fileName="examples/example1.txt"
 
-const loadPerElf = readFileSync(fileName, 'utf-8').split("\n\n").map((item) => (item.split('\n').map(Number).reduce((a,b)=>a+b)));
+const inputFile = readFileSync(fileName, 'utf-8')
+
+const loadPerElf = inputFile.split("\n\n").map((item) => (item.split('\n').map(Number).reduce((a,b)=>a+b)));
 
 // P1
 console.log("Part one:", Math.max(...loadPerElf));
