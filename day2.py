44968c05c4028e4b5df3e7e8dd598e116b915ec0
Eduardo <eduardoschiavo09@gmail.com>
2022-12-02 12:04:34 +0100

    refactoring

--- day2.py
+++ day2.py
@@ -1,67 +1,60 @@
-import enum
-
 with open('inputs/input2.txt') as ifile:
     inp=ifile.read().splitlines()
 
-# print(inp)
 
-# unify or convert to enum
-def opponent(letter: str)-> str:
+def parse_hand(letter: str)-> str:
     match letter:
-        case "A":
+        case "A" | "X":
             return 1
-        case "B":
+        case "B" | "Y":
             return 2
-        case "C":
+        case "C" | "Z":
             return 3
 
-def me(letter: str)-> str:
-    match letter:
-        case "X":
-            return 1
-        case "Y":
-            return 2
-        case "Z":
-            return 3
 
 def calculate_score(game: str)-> int:    
-
-    opponents_hand=opponent(game[0])
-    my_hand=me(game[2])
-
-    result = (opponents_hand, my_hand)
-    print(opponents_hand, my_hand)
+    result = (parse_hand(game[0]), parse_hand(game[2]))    
     # restructure 
-    if result[0]==result[1]:
-            # print("DRAW")
+    if result[0]==result[1]:     #DRAW
             return result[1] + 3
-    elif result[0]==1 and result[1]==3:
-            # print("opponent wins")
+    elif result[0]==1 and result[1]==3:  #LOOSE
             return result[1]
-    elif result[0]==3 and result[1]==1:
-            # print("I win")
+    elif result[0]==3 and result[1]==1:  #WIN
             return result[1] + 6
-    elif result[0] > result[1]:
-            # print("opponent wins")
+    elif result[0] > result[1]:          #LOOSE
             return result[1]
-    else:
-        print("i win")
+    else:                                #WIN
         return result[1] + 6
+    
+
+def calculate_score_p2(game: str)-> int:    
+    result = (parse_hand(game[0]), parse_hand(game[2]))    
+    match result[1]:
+        case 1:
+            if result[0] == 1:
+                return 3
+            elif result[0] == 2:
+                return 1
+            return 2
+        case 2:
+            return 3 + result[0]
+        case 3:
+            score = 6
+            if result[0] == 1:
+                return score + 2
+            elif result[0] == 2:
+                return score + 3
+            return score +1
 
     
 
-def compute_total_score(games: list)-> int:
-    total=0
-    for game in games:
-        total+=calculate_score(game)
-
-    return total
+# P1
+print("Part one: ", sum([calculate_score(game) for game in inp]))
 
 
-# print(inp[0])
-# print(calculate_score(inp[2]))
+# P2
+print("Part one: ", sum([calculate_score_p2(game) for game in inp]))
 
-print(compute_total_score(inp))
 
 
 
