68dcec4f9386885443e81c5a87493c53f1760830
Eduardo <eduardoschiavo09@gmail.com>
2023-12-05 18:38:30 +0100

    refactoring

--- 2023/day5.py
+++ 2023/day5.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 
-with open('examples/example5.txt') as ifile:
+with open('inputs/input5.txt') as ifile:
     inp=ifile.read()
 
 print(inp.split('\n\n'))
@@ -36,69 +36,21 @@ def parse_mapper(map_string: str):
     return [Instruction(*entry.split(' ')) for entry in map_string.split("\n")[1:]]
 
 
-
-#TODO refactor this shit
-SEED_TO_SOIL=parse_mapper(LINE_SEPARATED_INP[1])
-print(SEED_TO_SOIL)
-SOIL_TO_FERTILIZER=parse_mapper(LINE_SEPARATED_INP[2])
-print(SOIL_TO_FERTILIZER)
-FERTILIZER_TO_WATER=parse_mapper(LINE_SEPARATED_INP[3])
-print(FERTILIZER_TO_WATER)
-WATER_TO_LIGHT=parse_mapper(LINE_SEPARATED_INP[4])
-print(WATER_TO_LIGHT)
-LIGHT_TO_TEMPERATURE=parse_mapper(LINE_SEPARATED_INP[5])
-print(LIGHT_TO_TEMPERATURE)
-TEMPERATURE_TO_HUMIDITY=parse_mapper(LINE_SEPARATED_INP[6])
-print(TEMPERATURE_TO_HUMIDITY)
-HUMIDITY_TO_LOCATION=parse_mapper(LINE_SEPARATED_INP[7])
-print(HUMIDITY_TO_LOCATION)
+MAPS = [parse_mapper(LINE_SEPARATED_INP[i]) for i in range(1, 8)]
 
 
 def seed_path(seed: int)-> int:
     current = seed
-    for instruction in SEED_TO_SOIL:
-        current, updated=instruction.get_matching(current)
-        if updated: 
-            break
-    # print(current)
-    for instruction in SOIL_TO_FERTILIZER:
-        current, updated=instruction.get_matching(current)
-        if updated: 
-            break    
-    # print(current)
-    for instruction in FERTILIZER_TO_WATER:
-        current, updated=instruction.get_matching(current)
-        if updated: 
-            break
-    # print(current)
-    for instruction in WATER_TO_LIGHT:
-        current, updated=instruction.get_matching(current)
-        if updated: 
-            break    
-    for instruction in LIGHT_TO_TEMPERATURE:
-        current, updated=instruction.get_matching(current)
-        if updated: 
-            break    
-    for instruction in TEMPERATURE_TO_HUMIDITY:
-        current, updated=instruction.get_matching(current)
-        if updated: 
-            break
-    for instruction in HUMIDITY_TO_LOCATION:
-        current, updated=instruction.get_matching(current)
-        if updated: 
-            break    
+    for map in MAPS:
+        for instruction in map:
+            current, updated=instruction.get_matching(current)
+            if updated: 
+                break
     return current
-
-# print(seed_path(79)) #82
-# print(seed_path(14)) #43
-# print(seed_path(55)) #86
-# print(seed_path(13)) #35
 
 
 def p1():
     return min([seed_path(int(seed)) for seed in STARTING_SEEDS])
 
 
-print(p1())
-# print(FERTILIZER_TO_WATER[0].__dict__)
-# print(FERTILIZER_TO_WATER[1].get_matching(49))
+print(p1()) #282277027
