91edc3907f419159f39ff23b54f2943f374305d7
Eduardo <eduardoschiavo09@gmail.com>
2023-12-08 10:59:56 +0100

    refactoring

--- 2023/day8.py
+++ 2023/day8.py
@@ -8,44 +8,47 @@ INPUT = "inputs/input8.txt"
 with open(INPUT) as ifile:
     INP = ifile.read().splitlines()
 
-# print(INP)
 
 class Instruction(Enum):
     L = 0
     R = 1
 
-def parse_instructions_and_tree(input:list):
+
+def parse_instructions_and_tree(input: list):
     instructions = [Instruction[inst] for inst in input[0]]
-    nodes = {k: tuple(v.strip('()').split(', ')) for (k, v) in [entry.split(' = ') for entry in input[2::]]}
+    nodes = {k: tuple(v.strip('()').split(', '))
+             for (k, v) in [entry.split(' = ') for entry in input[2::]]}
     return instructions, nodes
 
 
-def execute_instructions(current, nodes, instructions, count: int):
-    for inst in instructions:
-        count +=1
-        current = nodes[current][inst.value]
-        if current[-1]=='Z': 
-            return count
-    return execute_instructions(current, nodes, instructions, count)
+INSTRUCTIONS, NODES = parse_instructions_and_tree(INP)
 
-def transverse(start_from, nodes, instructions):
+
+def execute_instructions(current, count: int):
+    for inst in INSTRUCTIONS:
+        count += 1
+        current = NODES[current][inst.value]
+        if current[-1] == 'Z':
+            return count
+    return execute_instructions(current, count)
+
+
+def transverse(start_from):
     current = start_from
-    count = 0 
-    return execute_instructions(current, nodes, instructions, count)
+    count = 0
+    return execute_instructions(current, count)
+
 
 def p1():
-    instr, nodes = parse_instructions_and_tree(INP)
-    print(transverse('AAA', nodes, instr))
+    print(transverse('AAA'))
 
-p1() #20093
 
-def get_starting_nodes(nodes):
-    return [key for key in nodes.keys() if key[-1]=='A']
+p1()  # 20093
+
 
 def p2():
-    instr, nodes = parse_instructions_and_tree(INP)
-    starting = get_starting_nodes(nodes)
-    print(lcm(*[transverse(starter, nodes, instr) for starter in starting ]))
+    print(lcm(*[transverse(starter)
+          for starter in [key for key in NODES.keys() if key[-1] == 'A']]))
 
-p2() #22103062509257
 
+p2()  # 22103062509257
