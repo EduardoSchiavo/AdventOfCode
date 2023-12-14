efaa0ef192744b1373b60211e8de8ce698aa65ee
Eduardo <eduardoschiavo09@gmail.com>
2023-12-14 07:39:12 +0100

    removed comments

--- 2023/day13.py
+++ 2023/day13.py
@@ -3,7 +3,6 @@ INPUT = "inputs/input13.txt"
 
 
 with open(INPUT) as ifile:
-    # INP = [ list(line) for line in ifile.read().splitlines()]
     INP_ROWWISE =  [pattern.split('\n') for pattern in ifile.read().split('\n\n')]
 
 def get_columns(input_as_rows):
@@ -17,22 +16,13 @@ def get_columns(input_as_rows):
 
 INP_COLUMNWISE = get_columns(INP_ROWWISE)
 
-# print(INP_ROWWISE)
-# print(INP_COLUMNWISE)
-
 
 def get_reflections(input_strings: list[str]):
     span = len(input_strings[0])+1
-    # reflections = []
     for i in range(1, len(input_strings)):
         if input_strings[i]==input_strings[i-1]:
-            # print(i, i-1)
-            # print(input_strings[i], input_strings[i-1])
-
             for j in range(1, span+1):
                 try:
-                    # print(j, span+1)
-                    # print(input_strings[i+j],input_strings[i-1-j])
                     if j==i:
                         return i
                     if not input_strings[i+j]==input_strings[i-1-j]:
@@ -41,20 +31,6 @@ def get_reflections(input_strings: list[str]):
                     return i 
     return 0
 
-
-
-print(INP_COLUMNWISE[0])
-print(get_reflections(INP_COLUMNWISE[0]))
-# assert get_reflections(INP_COLUMNWISE[0]), 5
-
-# print(INP_ROWWISE[0])
-# print(get_reflections(INP_ROWWISE[0]))
-# # assert get_reflections(INP_ROWWISE[0]), 0
-
-# print(INP_ROWWISE[1])
-# print(get_reflections(INP_ROWWISE[1]))
-
-# print(fold_horizontally(sample_map, 6))
 
 def p1():
     total = 0
@@ -66,3 +42,5 @@ def p1():
     return total
 
 print(p1())
+
+#Not sure yet how to solve for p2. Maybe look for strings that differ by 1?
