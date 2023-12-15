85a3e7436f626966ba30fd8de5fece163fc627f9
Eduardo <eduardoschiavo09@gmail.com>
2023-12-16 00:15:08 +0100

    day 15

--- 2023/day15.py
+++ 2023/day15.py
@@ -1,10 +1,13 @@
+from cProfile import label
+
+
 EXAMPLE = "examples/example15.txt"
 INPUT = "inputs/input15.txt"
 
 with open(INPUT) as ifile:
     INP = ifile.read().split(',')
 
-print(INP)
+# print(INP)
 
 def score_string(chars: str):
     value =0
@@ -14,7 +17,43 @@ def score_string(chars: str):
         value = value % 256
     return value
 
+def get_boxes_map(instructions: list[str])-> dict:
+    boxes = {}
+    for entry in instructions:
+        if '=' in entry:
+            label, focal_length = entry.split('=')
+            box_number = score_string(label)
+            if not boxes.get(box_number):
+                boxes[box_number]={label: focal_length}
+            else:
+                boxes[box_number][label] = focal_length
+        if '-' in entry:
+            label = entry[:-1]
+            box_number = score_string(label)
+            if boxes.get(box_number):
+                if label in boxes[box_number]:
+                    boxes[box_number].pop(label)
+    return boxes
+
+def score(boxes: dict)->int:
+    tot=0
+    for key, box in boxes.items():
+        for lab, lens in box.items():
+            tot+=int(lens)*(list(box).index(lab)+1)*(int(key)+1)
+    return tot
+
 def p1():
     return sum([score_string(i) for i in INP ])
 
-print(p1())
+def p2():
+    return score(get_boxes_map(INP))
+
+
+
+
+            
+
+# print(p1())
+
+print(p2()) #279116
+
