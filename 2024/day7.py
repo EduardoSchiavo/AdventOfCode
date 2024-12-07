d9574d8ff02c8f65aa04def1c12ad9e1c2adbab8
eduardoschiavo <eduardo.schiavo@domotz.com>
2024-12-07 16:07:59 +0100

    minor refactoring

--- 2024/day7.py
+++ 2024/day7.py
@@ -5,22 +5,20 @@ with open("inputs/input7.txt") as ifile:
            for line in ifile.read().splitlines() for k, v in [line.split(":")]]
 
 
-
-
 def is_valid(target: int, vs: list[int], ops: tuple[str]) -> bool:
     ops = list(ops)
     test_val = vs[0]
     for i, op in enumerate(ops):
         if op == '+':
             test_val += vs[i+1]
-        elif op =="*":
+        elif op == "*":
             test_val *= vs[i+1]
         else:
             test_val = int(str(test_val)+str(vs[i+1]))
     return test_val == target
 
 
-def sum_valid(eqs: list, ops="+*"):
+def sum_valid(eqs: list, ops: str):
     tot = 0
     for k, v in eqs:
         comb = itertools.product(ops, repeat=len(v)-1)
@@ -30,5 +28,11 @@ def sum_valid(eqs: list, ops="+*"):
                 break
     return tot
 
+def p1():
+    return sum_valid(INP, "+*")
 
-print(sum_valid(INP, "+*|"))
+def p2():
+    return sum_valid(INP, "+*|")
+
+print(p1())
+print(p2())
