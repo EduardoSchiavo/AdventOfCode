9828fdd2582158a6ebed36d754088323b192af1b
eduardoschiavo <eduardo.schiavo@domotz.com>
2024-12-08 14:56:58 +0100

    refactoring

--- 2024/day8.py
+++ 2024/day8.py
@@ -15,6 +15,7 @@ for a in ANTENNAS.keys():
 def midpoint(a: tuple, b: tuple) -> tuple:
     return (a[0]+b[0])/2, (a[1]+b[1])/2
 
+
 def p1():
     tot = 0
     for point in INP.keys():
@@ -29,25 +30,18 @@ def p1():
     return tot
 
 
-def compute_points(bounds: tuple, a: tuple, b: tuple)-> list[tuple]:
-    x = 0
-    points = []
-    while (x <= bounds[0]):
-        y = ((x-a[0])/(b[0]-a[0]))*(b[1]-a[1]) + a[1]
-        if y.is_integer() and 0<= y <=bounds[1]:
-            points.append((x,y))
-        x += 1
-    return points
+def compute_points(bounds: tuple, a: tuple, b: tuple) -> list[tuple]:
+    return [(x, y) for x in range(bounds[0] + 1) if (y := ((x-a[0])/(b[0]-a[0]))*(b[1]-a[1]) + a[1]).is_integer() and 0 <= y <= bounds[1]]
 
 
 def p2():
-    points = []
-    for k, v in ANTENNAS.items():
-        for i, num in enumerate(v[:-1]):
-            for j in v[i+1:]:
-                points.extend(compute_points(BOUNDS, num, j))
-    return len(set(points))
+    return len(set([
+        point for k, v in ANTENNAS.items()
+        for i, num in enumerate(v[:-1])
+        for j in v[i+1:]
+        for point in compute_points(BOUNDS, num, j)
+    ]))
+
 
 print(p1())
 print(p2())
-
