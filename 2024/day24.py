bcd01af412a2132dc08c3695e61a3ced60fe35b4
eduardoschiavo <eduardo.schiavo@domotz.com>
2025-01-07 10:07:39 +0100

    day24 p2 manual

--- 2024/day24.py
+++ 2024/day24.py
@@ -2,29 +2,32 @@ import operator
 
 with open("inputs/input24.txt") as ifile:
     W, G = ifile.read().split("\n\n")
-    W = {w.split(":")[0]:int(w.split(":")[1]) for w in W.splitlines()}
+    W = {w.split(":")[0]: int(w.split(":")[1]) for w in W.splitlines()}
     G = [tuple(g.split()) for g in G.splitlines()]
 
 
-# print(G)
-OPS= {'AND': operator.and_, 'OR': operator.or_, 'XOR': operator.xor}
+OPS = {'AND': operator.and_, 'OR': operator.or_, 'XOR': operator.xor}
+
 
 def propagate(wires: dict, gates: list):
-    # print(f"wires: {wires}")
     for gate in gates:
         if gate[0] in wires and gate[2] in wires:
             result = OPS[gate[1]](wires[gate[0]], wires[gate[2]])
-            # print(f"res: {result}")
             wires[gate[4]] = result
-            
+
+
 def p1(wires: dict):
     zeds = [g[4] for g in G if g[4].startswith('z')]
-    # print(zeds)
-    # print(all(z in W for z in zeds))
     while not all(z in W for z in zeds):
         propagate(wires, G)
-    return  int("".join([str(p[1]) for p in sorted({z: wires[z] for z in wires.keys() if z in zeds}.items())])[::-1], base =2)
+    return int("".join([str(p[1]) for p in sorted({z: wires[z] for z in wires.keys() if z in zeds}.items())])[::-1], base=2)
 
-# print(propagate(W, G))
-# print(W)
+# solved by manually checking the circuit :D
+def p2():
+    res = ['z09', 'nnf', 'z20', 'nhs', 'z34', 'wrc', 'ddn', 'kqh']
+    res.sort()
+    return ",".join(res)
+
+
 print(p1(W))
+print(p2())
