25e4e02a8af6ab32ae3f25ad40cee81c279df2c2
Eduardo <eduardoschiavo09@gmail.com>
2025-12-02 23:00:39 +0100

    reformatting

--- 2025/day2.py
+++ 2025/day2.py
@@ -28,28 +28,6 @@ def get_invalid_numbers(r: tuple[int]):
     return invalid_count
 
 
-def get_divisors(n: int):
-    divisors = [1]
-    for i in range(2, n):
-        if n % i == 0:
-            divisors.append(i)
-    return divisors
-
-
-def is_invalid(s: str):
-    if len(s) == 1:
-        return False
-    i = 0
-    divs = get_divisors(len(s))
-    while (i < len(divs)):
-        repeating = s[0:divs[i]]
-        multiplier = len(s)//divs[i]
-        if repeating * multiplier == s:
-            return True
-        i += 1
-    return False
-
-
 def get_invalid_numbers2(r: tuple[int]):
     invalid_count = 0
     for i in range(r[0], r[1]+1):
@@ -58,6 +36,26 @@ def get_invalid_numbers2(r: tuple[int]):
             invalid_count += int(stringified)
 
     return invalid_count
+
+
+def is_invalid(s: str):
+    if len(s) == 1:
+        return False
+    i = 0
+    divs = get_divisors(len(s))
+    while (i < len(divs)):
+        if s[0:divs[i]] * (len(s)//divs[i]) == s:
+            return True
+        i += 1
+    return False
+
+
+def get_divisors(n: int):
+    divisors = [1]
+    for i in range(2, n):
+        if n % i == 0:
+            divisors.append(i)
+    return divisors
 
 
 assert p1() == 17077011375
