145f3f20126bae6722ee6c5785298d30362a1bb1
Eduardo Schiavo <eduardo.schiavo@contentsquare.com>
2025-12-06 12:39:48 +0100

    format

--- 2025/day6.py
+++ 2025/day6.py
@@ -5,9 +5,19 @@ with open('inputs/input6.txt') as ifile:
     inp = ifile.read().splitlines()
 
 
-
 def p1():
     return sum(compute_problem(p) for p in parse_worksheet_h())
+
+
+def p2():
+    return sum(compute_problem(p) for p in parse_worksheet())
+
+
+def compute_problem(problem: list[str]) -> int:
+    if problem[-1] == "+":
+        return sum(problem[:-1])
+    return reduce(operator.mul, problem[:-1], 1)
+
 
 def parse_worksheet_h():
     rows = [[n for n in c.strip().split(" ") if n != ''] for c in inp]
@@ -18,14 +28,15 @@ def parse_worksheet_h():
         curr_prob = [int(rows[r][c]) for r in range(len(rows)-1)]
         curr_prob.append(op)
         problems.append(curr_prob)
-        curr_prob= []
+        curr_prob = []
     return problems
 
+
 def parse_worksheet():
-    problems= []
-    curr_prob= []
+    problems = []
+    curr_prob = []
     for i in reversed(range(len(inp[0]))):
-        curr_num= ''
+        curr_num = ''
         for j in range(len(inp)-1):
             curr_num += inp[j][i]
         if not curr_num.isspace():
@@ -33,19 +44,8 @@ def parse_worksheet():
         if inp[-1][i] in ('+', '*'):
             curr_prob.append(inp[-1][i])
             problems.append(curr_prob)
-            curr_prob= []
+            curr_prob = []
     return problems
-
-
-def compute_problem(problem: list[str]) -> int:
-    if problem[-1] == "+":
-        return sum(problem[:-1])
-    return reduce(operator.mul, problem[:-1], 1)
-
-def p2():
-    return sum(compute_problem(p) for p in parse_worksheet())
-
-
 
 
 assert p1() == 4719804927602
