516db8763de1c69f0c0f4bcce714d722391b9e76
Eduardo Schiavo <eduardo.schiavo@contentsquare.com>
2025-12-06 12:36:35 +0100

    refactoring

--- 2025/day6.py
+++ 2025/day6.py
@@ -1,74 +1,52 @@
+from functools import reduce
+import operator
+
 with open('inputs/input6.txt') as ifile:
     inp = ifile.read().splitlines()
 
-print(inp)
-# ROWS = [c.split(" ") for c in inp]
 
 
 def p1():
+    return sum(compute_problem(p) for p in parse_worksheet_h())
 
+def parse_worksheet_h():
     rows = [[n for n in c.strip().split(" ") if n != ''] for c in inp]
-    tot = 0
-    for c in range(len(rows[0])):
-        op = rows[-1][c]
-        if op == '+':
-            sm = sum(int(rows[r][c]) for r in range(len(rows)-1))
-            tot += sm
-        else:
-            prd = 1
-            for r in range(len(rows)-1):
-                prd *= int(rows[r][c])
-            tot += prd
-    return tot
-
-
-
-
-print(p1())
-
-
-def parse_worksheet():
-    for line in inp:
-        print(line)
-
     problems = []
     curr_prob = []
+    for c in range(len(rows[0])):
+        op = rows[-1][c]
+        curr_prob = [int(rows[r][c]) for r in range(len(rows)-1)]
+        curr_prob.append(op)
+        problems.append(curr_prob)
+        curr_prob= []
+    return problems
 
+def parse_worksheet():
+    problems= []
+    curr_prob= []
     for i in reversed(range(len(inp[0]))):
-        curr_num = ''
+        curr_num= ''
         for j in range(len(inp)-1):
-            # print(f"idxs: {i} {j} -- {inp[j][i]}")
             curr_num += inp[j][i]
         if not curr_num.isspace():
-            curr_prob.append(curr_num)
+            curr_prob.append(int(curr_num))
         if inp[-1][i] in ('+', '*'):
-            # print(f" op: {inp[-1][i]})")
             curr_prob.append(inp[-1][i])
-            # reset
             problems.append(curr_prob)
-            curr_prob = []
+            curr_prob= []
     return problems
 
 
 def compute_problem(problem: list[str]) -> int:
-
     if problem[-1] == "+":
-        return sum(int(n) for n in problem[:-1] if n != '')
-    prd = 1
-    for n in problem[:-1]:
-        if n == '':
-            continue
-        prd *= int(n)
-    return prd
+        return sum(problem[:-1])
+    return reduce(operator.mul, problem[:-1], 1)
 
 def p2():
-    problems = parse_worksheet()
-    tot = 0 
-    for prob in problems:
-        tot += compute_problem(prob)
-    return tot
+    return sum(compute_problem(p) for p in parse_worksheet())
 
 
 
-print(parse_worksheet())
-print(p2())
+
+assert p1() == 4719804927602
+assert p2() == 9608327000261
