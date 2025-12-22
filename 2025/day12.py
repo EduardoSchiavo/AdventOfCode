c13e3deea630d4f3aded81a644f25f7a57606059
Eduardo Schiavo <eduardo.schiavo@contentsquare.com>
2025-12-22 20:18:24 +0100

    day12 refactoring

--- 2025/day12.py
+++ 2025/day12.py
@@ -2,34 +2,21 @@ with open("inputs/input12.txt") as ifile:
     inp = ifile.read()
 
 
-
 *presents_s, spaces = inp.split("\n\n")
+
 
 PRESENTS = {}
 for p in presents_s:
     lines = p.split('\n')
-    PRESENTS[int(lines[0].strip(':'))] = lines[1:]
+    PRESENTS[int(lines[0].strip(':'))] = sum(c == '#' for row in lines[1:] for c in row)
 
-print(PRESENTS)
-
-print(spaces)
 
 REGIONS = []
 for s in spaces.strip('\n').split('\n'):
     size, boxes = s.split(': ')
     size = tuple(map(int, size.split('x')))
     boxes = tuple(map(int, boxes.split(' ')))
-    print(size, boxes)
     REGIONS.append((size, boxes))
-
-
-def occupies(box: list[str]):
-    tot = 0
-    for l in box:
-        for c in l:
-            if c == '#':
-                tot +=1
-    return tot
 
 
 def p1():
@@ -38,17 +25,10 @@ def p1():
         max_size = reg[0][0] * reg[0][1]
         occupied = 0
         for idx, qty in enumerate(reg[1]):
-            # print(idx, qty)
-            occupied += qty * occupies(PRESENTS[idx])
+            occupied += qty * PRESENTS[idx]
         if max_size >= occupied:
-            print(occupied)
             tot += 1
     return tot
 
 
-
-def p2():
-    pass
-
-
-print(f" p1: {p1()}")
+assert p1() == 403
